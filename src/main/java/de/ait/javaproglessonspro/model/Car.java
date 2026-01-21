25573a18cfa8699165d2c1b58a6f016699a106eb
Igor Zimmermann <igor.sonstnix@gmail.com>
2026-01-21 09:37:01 +0100

    lesson_07 additional fields and constructor

--- src/main/java/de/ait/javaproglessonspro/model/Car.java
+++ src/main/java/de/ait/javaproglessonspro/model/Car.java
@@ -1,12 +1,14 @@
 package de.ait.javaproglessonspro.model;
 
+
 import de.ait.javaproglessonspro.enums.CarStatus;
 import de.ait.javaproglessonspro.enums.FuelType;
 import de.ait.javaproglessonspro.enums.Transmission;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,46 +24,63 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Brand must not be empty")
     @Column(nullable = false)
+    @NotBlank(message = "Brand must not be empty")
     private String brand;
 
-    @NotBlank(message = "Model must not be empty")
     @Column(nullable = false)
+    @NotBlank(message = "Model must not be empty")
     private String model;
 
-    @Min(value = 1900, message = "Year must be 1900 or greater")
-    @Column(name = "production_year", nullable = false)
+    @Column(name = "production_year")
+    @Min(value = 1900, message = "Year must be greater than 1900")
     private int productionYear;
 
-    @Min(value = 0, message = "Mileage must be 0 or greater")
-    @Column(nullable = false)
+    @Min(value = 0, message = "Mileage must be greater than 0")
     private int mileage;
 
-    @Min(value = 1, message = "Price must be 1 or greater")
-    @Column(nullable = false)
+    @Min(value = 1, message = "Price must be greater than 0")
     private int price;
 
-    @NotNull(message = "Status must not be null")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private CarStatus status;
 
-    @NotNull(message = "FuelType must not be null")
     @Enumerated(EnumType.STRING)
-    @Column(name = "fuel_type", nullable = false)
+    @Column(nullable = false)
     private FuelType fuelType;
 
-    @NotNull(message = "Transmission must not be null")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Transmission transmission;
 
-    @NotBlank(message = "Color must not be empty")
     @Column(nullable = false)
+    @NotBlank(message = "Color must not be empty")
     private String color;
 
-    @Min(value = 1, message = "Horsepower must be 1 or greater")
     @Column(nullable = false)
+    @Min(value = 1, message = "Horsepower must be greater than 0")
     private int horsepower;
+
+
+    public Car(String brand, String model,
+               int productionYear,
+               int mileage,
+               int price,
+               String status,
+               String color,
+               int horsepower,
+               String fuelType,
+               String transmission) {
+        this.brand = brand;
+        this.model = model;
+        this.productionYear = productionYear;
+        this.mileage = mileage;
+        this.price = price;
+        this.status = CarStatus.valueOf(status);
+        this.color = color;
+        this.horsepower = horsepower;
+        this.fuelType = FuelType.valueOf(fuelType);
+        this.transmission = Transmission.valueOf(transmission);
+    }
 }
